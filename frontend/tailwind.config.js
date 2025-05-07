fdbd74c29418ea408fbbaee91789b6fdba26f3d3
Murial Anindo <murial.anindo@lonett.com>
2025-05-07 16:55:36 -0400

    adding frontend typography

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    // ...
+    extend: {
+    fontFamily: {
+        sans: ['var(--font-plus-jakarta)', 'sans-serif'],
+      },
+      fontSize: {
+        heading1: ['64px', { lineHeight: '72px' }],
+        subheading1: ['24px', { lineHeight: '32px' }],
+        subheading2: ['20px', { lineHeight: '28px' }],
+        paragraph1: ['20px', { lineHeight: '28px' }],
+        paragraph2: ['18px', { lineHeight: '26px' }],
+        paragraph3: ['16px', { lineHeight: '24px' }],
+        paragraph4: ['14px', { lineHeight: '20px' }],
+      },
+        fontWeight: {
+        semibold: 600,
+      },
+    },
   },
-  plugins: [
-    import('@tailwindcss/typography'),
-    // ...
-  ],
+  plugins: `[require('@tailwindcss/typography')],`
 }
+
