c6f1446176b5b12380f3d4b5e879f7f89ae472ed
Murial Anindo <murial.anindo@lonett.com>
2025-05-07 17:17:49 -0400

    added color schema

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#4B5563',
+        background: '#F9FAFB',
+        surface: '#FFFFFF',
+        text: {
+          dark: '#111827',
+          muted: '#6B7280',
+        },
+        border: '#E5E7EB',
+        accent: '#10B981',
+      }
+    }
+  },
+  darkMode: 'media', 
+}
