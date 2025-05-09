2f5097f0f99ac1c36696680cba0d6f7a3750ec4a
Murial Anindo <murial.anindo@lonett.com>
2025-05-09 12:48:46 -0400

    changes made to tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    extend: {
     fontFamily: {
-        sans: ['var(--font-plus-jakarta)', 'sans-serif'],
-      },
-      fontSize: {
-     heading1: ['4rem', { lineHeight: '4.5rem' }],     
-     subheading1: ['1.5rem', { lineHeight: '2rem' }],  
-     subheading2: ['1.25rem', { lineHeight: '1.75rem' }],
-     paragraph1: ['1.25rem', { lineHeight: '1.75rem' }],
-     paragraph2: ['1.125rem', { lineHeight: '1.625rem' }], 
-     paragraph3: ['1rem', { lineHeight: '1.5rem' }],  
-     paragraph4: ['0.875rem', { lineHeight: '1.25rem' }],
-      },
+      sans: ['var(--font-plus-jakarta)', 'sans-serif'],
     },
+    fontSize: {
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],     
+      base: ['1rem', { lineHeight: '1.5rem' }],         
+      md: ['1.125rem', { lineHeight: '1.625rem' }],    
+      lg: ['1.25rem', { lineHeight: '1.75rem' }],       
+      xl: ['1.5rem', { lineHeight: '2rem' }],         
+      '2xl': ['2rem', { lineHeight: '2.5rem' }],      
+      '3xl': ['3rem', { lineHeight: '3.5rem' }],        
+      '4xl': ['4rem', { lineHeight: '4.5rem' }],       
+    },
+    extend: {},
   },
-
 }
-
