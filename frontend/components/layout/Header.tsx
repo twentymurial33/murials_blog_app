1e1198433ecc8a6a0fbecec1ea8d476c27ede61f
Murial Anindo <murial.anindo@lonett.com>
2025-05-13 07:55:27 -0400

    fixing mr comments

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full bg-white shadow px-6 py-4 font-sans">
+    <header className="w-full bg-white shadow px-6 py-4">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <Link href="/" className="text-xl font-bold text-blue">
           Murial-Blog
         </Link>
         <nav className="flex gap-6 items-center text-darkGrey text-sm font-medium">
-          <Link href="/">Home</Link>
-          <Link href="/category">Category</Link>
-          <Link href="/about">About Me</Link>
+          <ul>
+            <li>  <Link href="/">Home</Link></li>
+            <li><Link href="/category">Category</Link></li>
+            <li><Link href="/about">About Me</Link></li>
+          </ul>
           <a
             href=""
             target="_blank"
