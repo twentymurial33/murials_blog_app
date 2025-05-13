2ed6e45e2d6d669d59b30f1dfa4df53072fbbabd
Murial Anindo <murial.anindo@lonett.com>
2025-05-12 20:03:10 -0400

    added header component

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 
-
 export default function Header() {
   return (
-    <header className="w-full bg-white shadow px-6 py-4">
+    <header className="w-full bg-white shadow px-6 py-4 font-sans">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <Link href="/" className="text-2xl font-bold text-blue-900">
+        <Link href="/" className="text-xl font-bold text-blue">
           Murial-Blog
         </Link>
-        <nav className="flex gap-6 items-center text-gray-700 text-sm font-medium">
+        <nav className="flex gap-6 items-center text-darkGrey text-sm font-medium">
           <Link href="/">Home</Link>
           <Link href="/category">Category</Link>
           <Link href="/about">About Me</Link>
@@ -16,7 +15,7 @@ export default function Header() {
             href=""
             target="_blank"
             rel="noopener noreferrer"
-            className="ml-4 inline-flex items-center bg-violet-600 text-white px-4 py-2 rounded-md text-sm hover:bg-violet-700 transition"
+            className="ml-4 inline-flex items-center bg-blue text-white px-4 py-2 rounded-md text-sm hover:opacity-90 transition"
           >
             ☕ Buy Me A Coffee
           </a>
