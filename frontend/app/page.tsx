33f79360f9a878e7b96083557708179111b849fc
Murial Anindo <murial.anindo@lonett.com>
2025-05-15 18:11:21 -0400

    first commit for category

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout/index";
+import CategoryList from "../components/categoryList";
 
 export default function Home() {
   return (
   
          <Layout >
             <h1>Hello World</h1>
+            <CategoryList/>
          </Layout>
 
   );
